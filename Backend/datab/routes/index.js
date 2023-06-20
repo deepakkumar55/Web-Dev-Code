b35b585aeba09d72d26cb6fde2badd7e5afb69f0
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-06-20 22:22:35 +0530

    .

--- Backend/datab/routes/index.js
+++ Backend/datab/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+const userModel = require("./users");
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/create', function(req, res, next) {
+  userModel.create({
+    username:"Aryan",
+    name:"Deepak Gupta",
+    email:"dk119819@gmail.co"
+  }).then(function(user){
+    res.send(user);
+  })
+});
+router.get('/find', function(req, res, next) {
+  userModel.find().then(function(alluser){
+    res.send(alluser);
+  })
+});
+router.get('/delete', function(req, res, next) {
+  userModel.findOneAndDelete({_id: "6491ab85044a5661344c54d2"}).then(function(aluser){
+    res.send(aluser);
+  })
 });
 
 module.exports = router;
