e7041faed6301d3ae3ba32ee552158a721b7fd81
Deepak Kumar <101995144+deepakkumar55@users.noreply.github.com>
2023-06-30 17:45:49 +0530

    .

--- Back-End/Kuchbhi/routes/index.js
+++ Back-End/Kuchbhi/routes/index.js
@@ -1,9 +1,76 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const mongoose = require('mongoose');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+mongoose.connect('mongodb://127.0.0.1:27017/social-media-platform', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  password: String
 });
 
-module.exports = router;
+const User = mongoose.model('User', userSchema);
+
+app.use(bodyParser.json());
+
+app.post('/api/register', (req, res) => {
+  const { username, password } = req.body;
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.error('Error hashing password:', err);
+      res.json({ success: false });
+    } else {
+      const user = new User({ username, password: hash });
+      user.save()
+        .then(() => {
+          res.json({ success: true });
+        })
+        .catch(error => {
+          console.error('Error saving user:', error);
+          res.json({ success: false });
+        });
+    }
+  });
+});
+
+app.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      console.error('Error finding user:', err);
+      res.json({ success: false });
+    } else if (!user) {
+      res.json({ success: false });
+    } else {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.error('Error comparing passwords:', err);
+          res.json({ success: false });
+        } else if (result) {
+          res.json({ success: true });
+        } else {
+          res.json({ success: false });
+        }
+      });
+    }
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
